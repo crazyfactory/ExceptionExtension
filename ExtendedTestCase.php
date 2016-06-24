359639cfd61859a13128cce1980452cdb05cbf0b
David Gray <dgray@crazy-factory.com>
2016-06-24 08:09:32 +0700

    Commitment is good

---
+++ ExtendedTestCase.php
@@ -0,0 +1,24 @@
+<?
+
+namespace CrazyFactory\PHPUnit;
+
+abstract class ExtendedTestCase extends \PHPUnit_Framework_TestCase
+{
+	protected function assertExceptionThrown($fn, $exceptionClass, $message = null)
+	{
+		$message = $message ?: ("should have thrown " . $exceptionClass);
+		$exception = null;
+
+		try
+		{
+			$fn();
+		}
+		catch (\Exception $e)
+		{
+			$exception = $e;
+		}
+
+		$this->assertThat($exception, new PHPUnit_Framework_Constraint_IsDerivedOf($exceptionClass), $message);
+	}
+}
+
